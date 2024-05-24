fa75e51b282049e9487dff879017ecfbd20f972c
jwu <jwu@bigquant.ai>
2024-05-24 11:23:13 +0800

    AIOS-2506 update date fmt support

--- src/extract_data_dai/__init__.py
+++ src/extract_data_dai/__init__.py
@@ -2,7 +2,8 @@
 
 import json
 import os
-from datetime import datetime, timedelta
+from datetime import timedelta
+import pandas as pd
 
 from jinja2 import Template
 import structlog
@@ -62,20 +63,8 @@ def run(
     if debug:
         logger.debug(sql)
 
-    # 自动解析日期时间的两种格式
-    def parse_date_fmt(date):
-        formats = ["%Y-%m-%d", "%Y-%m-%d %H:%M:%S"]
-        for fmt in formats:
-            try:
-                datetime.strptime(date, fmt)
-                return fmt
-            except ValueError:
-                continue
-        raise ValueError(f'时间格式不正确: {date}, 应为 "YYYY-MM-DD" 或 "YYYY-MM-DD HH:MM:SS"')
-
-    date_format = parse_date_fmt(start_date)
     if before_start_days >= 0:
-        query_start_date = (datetime.strptime(start_date, date_format) - timedelta(days=before_start_days)).strftime(date_format)
+        query_start_date = (pd.to_datetime(start_date) - timedelta(days=before_start_days)).strftime("%Y-%m-%d %H:%M:%S")
     else:
         query_start_date = start_date
 
